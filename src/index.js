181950b92bab6a2c4993be259ad90ebc1b77fe31
Petrik Diak <info@petrik.hu>
2022-11-15 14:42:54 +0100

    .

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
 import './style.css';
 
+function adatMegjelenites(idezetLista){
+    let lista = document.getElementById('idezetek');
+    for(p of eredmeny.quotes){
+        let li = document.createElement('li');
+        li.textContent = p.quote + '($' + p.author + ')';
+        lista.appendChild(li);    
+    }
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+    document.getElementById('osszes').addEventListener('click', async () => {
+        let response = await fetch('/quotes.json');
+        let eredmeny = await response.json();
+        let idezetek = eredmeny.quotes.filter(p => p.quote === '');
+        adatMegjelenites(idezetek);
+    })
+
+    document.getElementById('the').addEventListener('click', async () => {
+        let response = await fetch('/quotes.json');
+        let eredmeny = await response.json();
+        let idezetek = eredmeny.quotes.filter(p => p.quote === 'The');
+        adatMegjelenites(idezetek);
+    })
+    
+    let response = await fetch('/quotes.json');
+    let eredmeny = await response.json();
+
+    adatMegjelenites(eredmeny.quotes);
+});
